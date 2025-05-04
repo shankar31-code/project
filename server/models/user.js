5f33f1101661b0827a32685101f4808a63e3785d
shankar31-code <147817137+shankar31-code@users.noreply.github.com>
2025-05-04 20:22:23 +0530

    Update user.js user schema

--- server/models/user.js
+++ server/models/user.js
@@ -6,11 +6,13 @@ const userSchema =new mongoose.Schema({
     password:{type:String,required:true},
 });
 // this runs before saving to hash the password
-userSchema.pre('save',async function(next){
-    if(!this.isModified('password')) {return 
-    next();}
-    this.password=await bcrypt.hash(this.password,10);
-    next();
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
 });
+
  const User =mongoose.model('User',userSchema);
  export default User;
