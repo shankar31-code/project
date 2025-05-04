66bad2a840e1eb3e8ab5a3bfba5d58759a7718fd
shankar31-code <147817137+shankar31-code@users.noreply.github.com>
2025-05-04 19:12:48 +0530

    Update user.js bcrypt to bcryptjs

--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs'; // ✅ Changed from 'bcrypt' to 'bcryptjs'
+
 const userSchema =new mongoose.Schema({
     username:{ type:String,required:true,unique:true},
     password:{type:String,required:true},
