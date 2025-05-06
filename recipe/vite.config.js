0bdc0f1c6d46fe7376230a9fc19e0c76ec0322a1
shankar31-code <147817137+shankar31-code@users.noreply.github.com>
2025-05-06 15:17:25 +0530

    Update vite.config.js

--- recipe/vite.config.js
+++ recipe/vite.config.js
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig,loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+const env =loadEnv(mode,process.cwd());
 export default defineConfig({
   plugins: [react()],
   server:{
     proxy:{
       '/api':{
-        target: import.meta.env.VITE_API_URL,
+        target:env.VITE_API_URL,
         changeOrigin:true,
         secure:false,
       }
