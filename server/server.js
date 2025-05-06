cf647d6f5437467bfd64d830f21bb634c125d5fe
shankar31-code <147817137+shankar31-code@users.noreply.github.com>
2025-05-06 15:31:16 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-  origin: process.env.CLIENT_URL,
+  origin:'https://recipe-frontend-rysr.onrender.com',
   credentials: true
 }));
 
