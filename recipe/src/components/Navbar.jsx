ab6996a41bc949f41d515d60f36eeff43b92260c
shankar31-code <147817137+shankar31-code@users.noreply.github.com>
2025-05-12 15:19:03 +0530

    Update Navbar.jsx

--- recipe/src/components/Navbar.jsx
+++ recipe/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { useState, useEffect } from 'react';
+import { Menu, X } from 'lucide-react'; 
 
 const Navbar = ({ isAuth, setIsAuth, username, setUsername }) => {
   const [menuOpen, setMenuOpen] = useState(false);
